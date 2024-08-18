bf07b429dc3c63f76cced1d7915685b1527c0c0b
smb123w64gb <smb123w64gb@gmail.com>
2024-08-17 21:37:02 -0700

    Added a handy dandy build script

---
+++ psp_build.sh
@@ -0,0 +1,7 @@
+cd dependencies
+git clone https://github.com/smb123w64gb/RSDKv5-Decompilation.git --recursive -b PSP RSDKv5
+cd ..
+mkdir build
+cd build
+psp-cmake .. -DGAME_STATIC=ON -DRETRO_MOD_LOADER=OFF -DRETRO_REVISION=2 -DGAME_INCLUDE_EDITOR=OFF -DCMAKE_BUILD_TYPE=Release -DRETRO_AUDIO=OFF -DRETRO_VIDEO_CUTSCENE=OFF -DRETRO_DISABLE_PLUS=ON
+make -j$(nproc)
